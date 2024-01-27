4efa4f38c69f46776156f632acab0b30595f5736
Waad Bouzidi <82850473+waad641@users.noreply.github.com>
2024-01-27 19:36:19 +0100

    Update apps.py

--- consumer_project/consumer_app/apps.py
+++ consumer_project/consumer_app/apps.py
@@ -1,6 +1,11 @@
+# Import the AppConfig class from the django.apps module
 from django.apps import AppConfig
 
-
+# Create a configuration class for the 'consumer_app' Django app
 class ConsumerAppConfig(AppConfig):
+    # Specify the default auto-generated primary key field for models
     default_auto_field = 'django.db.models.BigAutoField'
+    
+    # Set the name of the Django app to 'consumer_app'
     name = 'consumer_app'
+
